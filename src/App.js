c4d4004b1dd2046f6e4d07cc7ae552a1c6155d55
Nicole Barranca <nikkibarranca@gmail.com>
2022-05-13 17:57:40 -0400

    App.js first edits

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import "./App.css";
+import React from "react";
+// might need App.css or import other css
+// might need to import photos
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.kjhfkjhksjhdfgjs</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
