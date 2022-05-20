c725c298f2712760c425653ef443428f2d0fab28
Nicole Barranca <nikkibarranca@gmail.com>
2022-05-20 15:04:05 -0400

    about me section styled

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,19 +5,25 @@ import Bit from "../../assets/bitmoji/Bit.png";
 
 function About() {
   return (
-    <div>
-      <h1>About me</h1>
+    <div className="about">
+      <h1 className="title">About me</h1>
       <img src={Bit} style={{ width: "20%" }} alt="bitmoji" />
       <p>
         My name is name is Nicole Barranca and I am from central New Jersey. I
         am a Full Stack Software Developer, with a background in Web Design.
-        Most of the time you will will find me at my computer learning new
-        languages, and technologies while drinking a mega pint of coffee. I
-        thoroughly enjoy the process of building, collaborating and completing a
-        project. I am passionate about problem solving and when it comes to
-        bugs, I CANNOT BE STOPPED! Take a look at some of my most recent
-        applications in the portfolio section. Thanks for stopping by!
       </p>
+      <p>
+        I thoroughly enjoy the process of building, collaborating and completing
+        a project. Most of the time you will will find me at my computer
+        learning new languages, and technologies while drinking a mega pint of
+        coffee. I am passionate about problem solving and when it comes to bugs,
+        I cannot be stopped!
+      </p>
+      <p>
+        Take a look at some of my most recent applications in the portfolio
+        section.
+      </p>
+      <p>Thanks for stopping by!</p>
     </div>
   );
 }
